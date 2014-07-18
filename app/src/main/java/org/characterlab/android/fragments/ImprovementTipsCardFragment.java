943237c49b1b8380b956431b245ec64eb5ccbb8d
Mandar Bapaye <mandarbapaye@yahoo.com>
2014-07-17 18:58:29 -0700

    Changes to show a detailed improvement tips card when clicked on the smaller version in student details

--- app/src/main/java/org/characterlab/android/fragments/ImprovementTipsCardFragment.java
+++ app/src/main/java/org/characterlab/android/fragments/ImprovementTipsCardFragment.java
@@ -1,11 +1,15 @@
 package org.characterlab.android.fragments;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.text.Html;
+import android.view.ContextThemeWrapper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import org.characterlab.android.R;
@@ -22,6 +26,8 @@ public class ImprovementTipsCardFragment extends Fragment {
     private int pageNumber;
     private int pageCount;
     private Strength strength;
+
+    private RelativeLayout rlImproveTipsFrag;
 
     public static ImprovementTipsCardFragment newInstance(int pageNumber, int pageCount, Strength strength) {
         ImprovementTipsCardFragment fragment = new ImprovementTipsCardFragment();
@@ -48,6 +54,8 @@ public class ImprovementTipsCardFragment extends Fragment {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_improve_tips_card, container, false);
 
+        rlImproveTipsFrag = (RelativeLayout) v.findViewById(R.id.rlImproveTipsFrag);
+
         TextView tvTipsCardPageCount = (TextView) v.findViewById(R.id.tvTipsCardPageCount);
         String pageCountStr = pageNumber + "/" + pageCount;
         tvTipsCardPageCount.setText(pageCountStr);
@@ -56,7 +64,7 @@ public class ImprovementTipsCardFragment extends Fragment {
         String tipsCardTitle = "Improve " + strength.getName();
         tvTipsCardLabel.setText(tipsCardTitle);
 
-        StrengthInfo strengthInfo = StrengthInfo.fromStrength(getActivity(), strength);
+        final StrengthInfo strengthInfo = StrengthInfo.fromStrength(getActivity(), strength);
         List<StrengthInfoItem> infoItems = strengthInfo.getBuildItems();
         int count = 1;
         for (StrengthInfoItem infoItem : infoItems) {
@@ -87,8 +95,51 @@ public class ImprovementTipsCardFragment extends Fragment {
             count++;
         }
 
+        rlImproveTipsFrag.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showImprovementTipsDialog(strengthInfo);
+            }
+        });
+
+
         return v;
     }
+
+
+    private void showImprovementTipsDialog(StrengthInfo strengthInfo) {
+        final ContextThemeWrapper context = new ContextThemeWrapper(getActivity(),
+                android.R.style.Theme_Holo_Dialog);
+
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View v = inflater.inflate(R.layout.strenth_improvement_tips_dialog, null);
+
+        TextView tvStrengthImprovementDialog = (TextView) v.findViewById(R.id.tvStrengthImprovementDialog);
+        TextView tvStrengthImprovementDialogHeader = (TextView) v.findViewById(R.id.tvStrengthImprovementDialogHeader);
+
+        StringBuilder strBuilder = new StringBuilder();
+        if (strengthInfo != null) {
+            tvStrengthImprovementDialogHeader.setText("Improve " + strengthInfo.getName());
+            List<StrengthInfoItem> infoItems = strengthInfo.getBuildItems();
+            for (StrengthInfoItem infoItem : infoItems) {
+                strBuilder.append("<b>");
+                strBuilder.append(infoItem.getTitle());
+                strBuilder.append("</b>");
+                strBuilder.append(infoItem.getContents());
+            }
+        }
+        tvStrengthImprovementDialog.setText(Html.fromHtml(strBuilder.toString()));
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setView(v)
+                .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                    }
+                })
+                .create().show();
+    }
+
 
 
 

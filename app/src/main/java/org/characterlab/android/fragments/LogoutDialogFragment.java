a038f4edbc385d0a07c9089be4f8e7e2ef60e2f6
Tina Wen <tina@dropbox.com>
2014-07-18 23:31:33 -0700

    made logout screen a lot nicer

--- app/src/main/java/org/characterlab/android/fragments/LogoutDialogFragment.java
+++ app/src/main/java/org/characterlab/android/fragments/LogoutDialogFragment.java
@@ -1,10 +1,12 @@
 package org.characterlab.android.fragments;
 
-import android.app.AlertDialog;
-import android.app.Dialog;
-import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.Button;
 
 import com.parse.ParseUser;
 
@@ -19,33 +21,36 @@ public class LogoutDialogFragment extends DialogFragment {
         void onLogoutFragmentSuccess();
     }
 
-
     @Override
-    public Dialog onCreateDialog(Bundle savedInstanceState) {
-        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(getActivity());
-        alertDialogBuilder.setMessage(R.string.logout_warning_text);
-        alertDialogBuilder.setPositiveButton(R.string.logout_action,  new DialogInterface.OnClickListener() {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_sign_out, container);
+        Button signOutButton = (Button) view.findViewById(R.id.btnSignOut);
+        signOutButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(DialogInterface dialog, int which) {
-            ParseUser.logOut();
-            ParseUser currentUser = ParseUser.getCurrentUser();
-            if (currentUser == null) {
-                try {
-                    LogoutFragmentListener logoutFragmentListener = (LogoutFragmentListener) getActivity();
-                    logoutFragmentListener.onLogoutFragmentSuccess();
-                } catch (ClassCastException exception) {
-                    exception.printStackTrace();
+            public void onClick(View view) {
+                ParseUser.logOut();
+                ParseUser currentUser = ParseUser.getCurrentUser();
+                if (currentUser == null) {
+                    try {
+                        LogoutFragmentListener logoutFragmentListener = (LogoutFragmentListener) getActivity();
+                        logoutFragmentListener.onLogoutFragmentSuccess();
+                        getDialog().dismiss();
+                    } catch (ClassCastException exception) {
+                        exception.printStackTrace();
+                    }
                 }
             }
-            }
         });
-        alertDialogBuilder.setNegativeButton(R.string.cancel_action, new DialogInterface.OnClickListener() {
+         Button cancelButton = (Button) view.findViewById(R.id.btnCancel);
+        cancelButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(DialogInterface dialog, int which) {
-                dialog.dismiss();
+            public void onClick(View view) {
+                getDialog().dismiss();
             }
         });
+        getDialog().requestWindowFeature(Window.FEATURE_NO_TITLE);
 
-        return alertDialogBuilder.create();
+        return view;
     }
 }
